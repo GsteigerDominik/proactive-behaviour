b95e81bba61890c01456ec28d88213b08152debf
GsteigerDominik <dominik.gsteiger@gmail.com>
2025-04-13 13:46:54 +0200

    fix agent.py

--- app/agent.py
+++ app/agent.py
@@ -18,7 +18,7 @@ def react(chat_id):
 
 def should_answer(chat_history):
     base_prompt = file_util.load_txt_file("./app/decision_prompt.txt")
-    prompt = base_prompt.format(timestamp=datetime.now(ZoneInfo("Europe/Zurich"), chat_history=chat_history))
+    prompt = base_prompt.format(timestamp=datetime.now(ZoneInfo("Europe/Zurich")), chat_history=chat_history)
     response: response_formats.DecisionResponse = openai_util.gpt_query_with_response_format(prompt,
                                                                                              response_formats.DecisionResponse)
     return response
@@ -26,6 +26,6 @@ def should_answer(chat_history):
 
 def generate_response(chat_history):
     base_prompt = file_util.load_txt_file("./app/text_prompt.txt")
-    prompt = base_prompt.format(timestamp=datetime.now(ZoneInfo("Europe/Zurich"), chat_history=chat_history))
+    prompt = base_prompt.format(timestamp=datetime.now(ZoneInfo("Europe/Zurich")), chat_history=chat_history)
     response = openai_util.gpt_query(prompt)
     return response
